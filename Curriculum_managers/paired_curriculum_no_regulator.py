5a8dcbb596d202792734a000227210deb0b09b76
nitsan57 <nitsan57@gmail.com>
2022-01-10 20:57:08 +0200

    small bugfix

--- Curriculum_managers/paired_curriculum_no_regulator.py
+++ Curriculum_managers/paired_curriculum_no_regulator.py
@@ -12,7 +12,7 @@ import operator
 class Curriculum_Entropy_Only(Curriculum_Manager):
     def __init__(self, abstract_env, trainee, teacher_agent, save_dir=None) -> None:
         if save_dir is None:
-            save_dir = "./results/PAIRED_Curriculum_no_regret_entropy/" + abstract_env.__class__.__name__ + "/"
+            save_dir = "./results/Curriculum_Entropy_Only/" + abstract_env.__class__.__name__ + "/"
         
         self.random_z_dim = (10,)
         self.teacher = teacher_agent
